fd34d9529b9e51b61de6309f21628d9051234ad9
richardhaddadau <richard@haddads.net.au>
2021-10-17 04:00:16 +1000

    Added Resolving of Parentheses

--- js/app.js
+++ js/app.js
@@ -81,23 +81,74 @@ let calculationObj = {
         // Process 1: Split by Addition         [(14 + 4) - 5 * 3, 16 / 2 - 5]
         // Process 2: Split by Subtraction      [(14 + 4), 5 * 3], [16 / 2, 5]
         // Process 3: Split by Division         [(14 + 4)], [5 * 3], [16, 2], [5]
-        // Process 4: Split by Multiplication   [14, 3], [16], [2], [5]
+        // Process 4: Split by Multiplication   [(14 + 3)], [5, 3], [16], [2], [5]
         // Process 5: Split by Parentheses      []
 
         calculationObj.ongoingTotal = calculationObj.resolveAddition(expression);
     },
 
+    resolveParentheses: (expression, operator) => {
+        // Set required required:
+        // Parentheses Array to produce a new array for parsing
+        // Parentheses Expression to store ongoing expression
+        // Parentheses Tracker to check if we're in a parentheses block
+        let prthsTracker = 0;
+        let prthsExpression = '';
+        let prthsArr = [];
+
+        // Loop through the main expression passed in
+        for (let count = 0; count < expression.length; count++) {
+
+            // Track Parentheses
+            // Originally I used a boolean value for Parentheses Tracker but this only allows for one level of parentheses
+            // Using integers instead, allows for multiple levels of parentheses
+            if (expression[i] == '(') {
+                prthsTracker++;
+            } else if (expression[i] == ')') {
+                prthsTracker--;
+            }
+
+            // Check if Parentheses Tracker is back to 0 and if the current expression is the Operator we're parsing
+            if (prthsTracker == 0 && expression[i] == operator) {
+
+                // Push Parentheses Expression to the Parentheses Array
+                prthsArr.push(prthsExpression);
+
+                // Reset Parantheses Expression
+                prthsExpression = '';
+
+            } else {
+
+                // If not, keep adding each character in the main expression to the ongoing expression
+                prthsExpression += expression[i];
+
+            }
+
+        }
+
+        // When we have reached the end of the expression
+        // Check if there's anything in our ongoing expression
+        if (prthsExpression != '') {
+            
+            // Add our ongoing expression to our Parentheses Array
+            prthsArr.push(prthsExpression);
+
+        }
+
+        return prthsArr;
+    },
+
     resolveAddition: (expression) => {
         // Create an Array by splitting the Expression using the Plus Symbol as the splitter
-        let stringArr = expression.split('+');
+        // However, rather than use the JavaScript Array Split method
+        // We use our own function which splits and strips from parentheses
+        let stringArr = resolveParentheses(expression, '+');
 
+        // Parse each value in our new split and stripped array
         let numberArr = stringArr.map(x => calculationObj.resolveSubtraction(x));
 
-        // Set an Initial Value of 0 for the Reduce Method
-        const initialValue = 0.0;
-
         // Process a Reduce method on our array adding each pair in the array
-        let resolvedResult = numberArr.reduce((prev, current) => prev + current, initialValue);
+        let resolvedResult = numberArr.reduce((prev, current) => prev + current);
 
         // Return our resolved result
         return resolvedResult;
@@ -105,8 +156,10 @@ let calculationObj = {
 
     resolveSubtraction: (expression) => {
         // Create an Array by splitting the Expression using the Minus Symbol as the splitter
-        let stringArr = expression.split('-');
+        // Using our own function which splits and strips from parentheses
+        let stringArr = resolveParentheses(expression, '-');
 
+        // Parse each value in our new split and stripped array
         let numberArr = stringArr.map(x => calculationObj.resolveDivision(x));
 
         // Process a Reduce method on our array subtracting each pair in the array
@@ -118,8 +171,10 @@ let calculationObj = {
 
     resolveDivision: (expression) => {
         // Create an Array by splitting the Expression using the Division Symbol as the splitter
-        let stringArr = expression.split('/');
+        // Using our own function which splits and strips from parentheses
+        let stringArr = resolveParentheses(expression, '/');
 
+        // Parse each value in our new split and stripped array
         let numberArr = stringArr.map(x => calculationObj.resolveMultiplication(x));
 
         // Process a Reduce method on our array dividing each pair in the array
@@ -131,15 +186,25 @@ let calculationObj = {
 
     resolveMultiplication: (expression) => {
         // Create an Array by splitting the Expression using the Multiplication Symbol as the splitter
-        let stringArr = expression.split('*');
+        // Using our own function which splits and strips from parentheses
+        let stringArr = resolveParentheses(expression, '*');
 
-        let numberArr = stringArr.map(x => parseFloat(x));
+        // Parse each value in our new split and stripped array
+        let numberArr = stringArr.map(x => {
 
-        // Set an Initial Value of 1 for the Reduce Method
-        const initialValue = 1.0;
+            // If current value in the array is an open parantheses
+            // Strip current level parentheses and then run parsing again from Addition
+            if (x[0] == '(') {
+
+                return resolveAddition(x.subsr(1, x.length - 2));
+
+            }
+            
+            return parseFloat(x);
+        });
 
         // Process a Reduce method on our array multiplying each pair in the array
-        let resolvedResult = numberArr.reduce((prev, current) => prev * current, initialValue);
+        let resolvedResult = numberArr.reduce((prev, current) => prev * current);
 
         // Return our resolved result
         return resolvedResult;
@@ -156,17 +221,32 @@ let calculationObj = {
     }
 };
 
+// Toggle Light/Dark Mode
 const changeToDark = (option) => {
-    lightUrl = "css/light.css";
-    darkUrl = "css/dark.css";
+    // NEW UPDATED CODE
+    // Create an Object for CSS Files
+    const urlObj = {
+        0: "css/light.css",
+        1: "css/dark.css"
+    };
 
-    switch (option) {
-        case 0:
-            document.querySelector('#dark-mode-css').setAttribute("href", lightUrl);
-            break;
+    // Change the CSS link href attribute to the chosen option
+    document.querySelector('#dark-mode-css').setAttribute("href", urlObj(option));
+
     
-        default:
-            document.querySelector('#dark-mode-css').setAttribute("href", darkUrl);
-            break;
-    }
+    // OLD ARCHIVED CODE
+    // Set CSS Files to Variables
+    // const lightUrl = "css/light.css";
+    // const darkUrl = "css/dark.css";
+    
+    // Use Switch/Case to process this.
+    // switch (option) {
+    //     case 0:
+    //         document.querySelector('#dark-mode-css').setAttribute("href", lightUrl);
+    //         break;
+    
+    //     default:
+    //         document.querySelector('#dark-mode-css').setAttribute("href", darkUrl);
+    //         break;
+    // }
 };
